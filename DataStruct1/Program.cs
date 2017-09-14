bb4466977407059784ff029946126b150f46b7e5
andy <ANDY@byu766618wks.app.byu.edu>
2017-09-14 08:03:46 -0600

    In class - V1.0

---
+++ DataStruct1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataStruct1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] aiNums = new int[3];
+            int aiNumsCount = aiNums.Length;
+            aiNums[0] = 1;
+            aiNums[1] = 2;
+            aiNums[2] = 3;
+            
+
+            for (int i = 0; i < aiNumsCount; i++)
+            {
+                Console.WriteLine(aiNums[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
